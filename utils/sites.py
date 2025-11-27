6360a517a8c06b8f07a33f02b2bbaace3a6a0c3f
alfarisgrp-source <source.control@alfarisgrp.com>
2025-11-27 09:24:36 +0300

    sites mapping change

--- utils/sites.py
+++ utils/sites.py
@@ -1,33 +1,41 @@
 site_user_map = {
+    # Main FG Warehouse Sites
     "Khamis": "RPA0001",
+    "Bisha": "RPA0001",
+    "Mahiel": "RPA0001",
+    "Mahayel": "RPA0001",
     "Najran": "RPA0001",
-    "Sharoura":"RPA0001",  
-    "B2B": "RPA0001", 
-    "Qonfoda": "RPA0001", 
-    "Qatif": "RPA0001" ,
-    "Alnamas": "RPA0001", 
-    "Sabth Al -Alaya": "RPA0001", 
-    "Baha": "RPA0001" ,
-    "Abha": "RPA0001" ,
+    "Sharoura": "RPA0001",
+    "B2B": "RPA0001",
+    "Qonfoda": "RPA0001",
+    "Alnamas": "RPA0001",
+    "Sabth Al -Alaya": "RPA0001",
+    "Baha": "RPA0001",
+    "Abha": "RPA0001",
 
-    "Madinah": "RPA0002",  
-    "Alqaseem": "RPA0003",
-    "Bisha": "RPA0004",
 
-    "Jeddah": "RPA0005", 
-    "Taif": "RPA0005", 
-    "Makkah": "RPA0005", 
+    # Jeddah Main Warehouse Sites
+    "Jeddah": "RPA0005",  
+    "Madinah": "RPA0005",
+    "Taif": "RPA0005",
+    "Makkah": "RPA0005",
 
+    # Jazan Main Warehouse Sites 
     "Jazan": "RPA0006",
-    "Mahiel": "RPA0007",
-    "Mahayel": "RPA0007",
+
+    # Namrah Main Warehouse Sites
     "Namrah": "RPA0008",
 
+    # Riyadh Main Warehouse Sites
     "Riyadh": "RPA0009",
+    "Qassem": "RPA0009",
+    "Qatif": "RPA0009",
     "Hayil": "RPA0009",
     "Jubail": "RPA0009",
     "RiyadhQatif": "RPA0009",
     "Khobar": "RPA0009",
     "Dammam": "RPA0009",
+    "Qwaia": "RPA0009",
+    "Muzahmiyah": "RPA0009",
 }
     
